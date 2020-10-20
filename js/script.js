a993ccb6e70497fff546a398615156fcb1a33a9b
Smolenko <harvestter18@gmail.com>
2020-10-20 19:39:44 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "");
+//console.log(typeof(answer));
+
+//const answers = [];
+  
+//answers[0] = prompt(name?, );
+//answers[1] = prompt('surname?', '');
+///answers[2] = prompt('old?', '');
+
+//document.write(answers);
+
+console.log('arr' + "- object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+--incr;
+++decr;
+
+console.log(incr);
+console.log(decr);
+
+
