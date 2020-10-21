ce98d18f86a85949a1bfe00cefcad984bb044ff1
Smolenko <harvestter18@gmail.com>
2020-10-21 17:01:32 +0200

    1.quations

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 "use strict";
 
-//alert("Hello");
+/*
+//Вопросы пользователю и запись их в пустые объекты
+const numberOfFilms = +prompt('How many movies do you watch?', '');
 
-//const result = confirm("Are you here?");
-//console.log(result);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-//const answer = prompt("Вам есть 18?", "");
-//console.log(typeof(answer));
+const a = prompt('one of the last viewed movies', ''),
+      b = prompt('how much do you rate it for', ''),
+      c = prompt('one of the last viewed movies', ''),
+      d = prompt('how much do you rate it for', '');
 
-//const answers = [];
-  
-//answers[0] = prompt(name?, );
-//answers[1] = prompt('surname?', '');
-///answers[2] = prompt('old?', '');
-
-//document.write(answers);
-
-console.log('arr' + "- object");
-console.log(4 + "5");
-
-let incr = 10,
-    decr = 10;
-
---incr;
-++decr;
-
-console.log(incr);
-console.log(decr);
-
+personalMovieDB.movies[a] = b; //Добавление свойств в пустой объект
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);*/
 
