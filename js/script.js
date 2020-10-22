8e6b127a0af6fac889da89d32b7f141bc37b8c5a
Smolenko <harvestter18@gmail.com>
2020-10-22 16:02:56 +0200

    loop for

--- js/script.js
+++ js/script.js
@@ -21,3 +21,42 @@ personalMovieDB.movies[a] = b; //Добавление свойств в пуст
 personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);*/
 
+const numberOfFilms = +prompt('How many movies do you watch?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+/*
+// Цикл FOR с проверкой того что пользователь ввел корректные данные
+
+for (let i = 0; i < numberOfFilms; i++) {
+
+    let a = prompt('one of the last viewed movies', ''),
+        b = prompt('how much do you rate it for', '');
+    
+    if ( a != null && b != null && a != '' && b != "" && a.length < 50 ) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log("error");
+        i--;
+    }      
+}
+ 
+if (personalMovieDB.count < 10) {
+    console.log('viewed a few movies');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('you are a classic viewer');
+} else if (personalMovieDB.count >= 30) {
+    console.log('are you a movie fan');
+} else {
+    console.log('misstake');
+}
+
+
+console.log(personalMovieDB);*/
