10a77bca09eec598c2169a762a5eb0ebe6a17101
Smolenko <harvestter18@gmail.com>
2020-10-29 18:16:11 +0100

    ddddddd

--- js/work.js
+++ js/work.js
@@ -4,7 +4,29 @@ const box = document.getElementById('box'),
       btns = document.getElementsByTagName('button'),
       circles = document.getElementsByClassName('circle'),
       hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart');
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
 
 
-box.style.
+//box.style.backgroundColor = 'blue';
+//box.style.width = '500px';
+
+box.style.cssText = `background-color: blue; width: 500px`;
+
+btns[1].style.borderRadius = '50%';
+
+circles[0].style.backgroundColor = 'red';
+
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+div.classList.add('black');
+document.body.append(div); 
+
+wrapper.append(div);
+div.innerHTML = '<h1>Hello</h1>';
+
+div.insertAdjacentHTML('beforeend', '<h1>WORLD</h1>')
