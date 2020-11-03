98fbd5f7a1c16f23dab0b3abd16fb6322886481f
Smolenko <harvestter18@gmail.com>
2020-11-03 11:47:23 +0100

    fefe

--- js/work.js
+++ js/work.js
@@ -1,59 +1,38 @@
 "use strict";
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart'),
-      wrapper = document.querySelector('.wrapper');
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
-//box.style.backgroundColor = 'blue';
-//box.style.width = '500px';
-
-box.style.cssText = `background-color: blue; width: 500px`;
-
-btns[1].style.borderRadius = '50%';
-
-circles[0].style.backgroundColor = 'red';
-
-
-hearts.forEach(item => {
-    item.style.backgroundColor = 'blue';
+adv.forEach(item => {
+    item.remove();
 });
 
-const div = document.createElement('div');
-div.classList.add('black');
-document.body.append(div); 
+genre.textContent = 'драма';
 
-wrapper.append(div);
-div.innerHTML = '<h1>Hello</h1>';
+poster.style.backgroundImage = 'url("img/bg.jpg")';
 
-div.insertAdjacentHTML('beforeend', '<h1>WORLD</h1>')
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
 
 
-
-
-
-function rot13(str) {
-let strNew = [];
-let arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-for(let i=0; i < str.length; i++) {
-    if(str[i] == " "|str[i] == "!"|str[i] == "?"|str[i] == ".") {
-        strNew.push(str[i]);
-    } else {
-        for(let j=0; j < arr.length; j++) {
-            if(arr[j] == str[i] && j >= 13) {
-            strNew.push(arr[j - 13]);   
-            } else if (arr[j] == str[i]) {
-                strNew.push(arr[j + 13]);    
-            }
-        }
-        
-    }
-}
-
-    return strNew.join('');
-  }
-  
-  console.log(rot13("SERR CVMMN!"));
